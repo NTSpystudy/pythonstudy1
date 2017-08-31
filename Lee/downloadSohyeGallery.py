5874c9e2e103513bdcd17182e4f552a722ee76c7
redtime2002@naver.com <redtime2002@naver.com>
2017-08-31 18:57:32 +0900

    소혜

--- Lee/downloadSohyeGallery.py
+++ Lee/downloadSohyeGallery.py
@@ -1,10 +1,8 @@
 import random
 
-import urllib
-<<<<<<< HEAD
-=======
+import html
 import urllib.request
->>>>>>> f11705b1a51c00f32cebc848dc6316266bd6317c
+import lxml
 from bs4 import BeautifulSoup as BS
 
 from operator import eq
@@ -21,7 +19,6 @@ def download_web_images(url):
 
     name = random.randrange(1,1001)
     full_name = str(name) + ".jpg"
-<<<<<<< HEAD
 
     img_req = urllib.request.Request(url, data=None, headers={'User-Agent': user_agent});
     urllib.request.urlretrieve(img_req, "./img/" + full_name)
@@ -42,59 +39,93 @@ def download_web_images_no_extension(url):
     # f = open( "./img/" + str(name) + '.jpg', 'wb')
     # f.write(urllib.request.urlopen(url).read())
     # f.close()
-=======
-    urllib.request.urlretrieve(url, "./img/" + full_name)
->>>>>>> f11705b1a51c00f32cebc848dc6316266bd6317c
 
 def download_web_images_noExtension(url):
-
 
     name = random.randrange(1, 1001)
     f = open( "./img/" + str(name) + '.jpg', 'wb')
     f.write(urllib.request.urlopen(url).read())
     f.close()
 
+def find_by_xpath(element_source,xpath_expression):
+    root = html.fromstring(element_source)
+    return root.xpath(xpath_expression)
+
+
+
 if __name__ == "__main__":
 
     driver = webdriver.Chrome("chromedriver.exe")
     driver.get("http://gall.dcinside.com/board/lists/?id=kimsohye")
-    soup = BS(driver.page_source, 'html5lib')
+    soup = BS(driver.page_source, 'html.parser')
 
     # f = open("./crawlWebpage.html", "wb")
     # f.write(soup.prettify().encode())
     # f.close()
 
-    for link in soup.findAll("a"):
-        if ('href' in link.attrs and 'class' in link.attrs):
-            if(eq(str(link.attrs['class']),'[\'icon_pic_n\']')):
-                print(link.attrs['href'])
-<<<<<<< HEAD
-                driver.find_element_by_xpath("// *[@href = \"" + str(link.attrs['href']) + "\"]").click()
-=======
-                driver.find_element_by_xpath("// *[@href = \"" + link.attrs['href'] + "\"]").click()
->>>>>>> f11705b1a51c00f32cebc848dc6316266bd6317c
+    idx = 0
 
-                time.sleep(1)
+    for s in soup.find_all("a"):
 
-                soup = BS(driver.page_source, 'html5lib')
-<<<<<<< HEAD
-                imgs = driver.find_elements_by_xpath(
-                    "// *[ @ id = 'dgn_content_de'] / div[2] / div[1] / div / table / tbody / tr / td / div / img")
-=======
-                imgs = driver.find_elements_by_xpath("// *[ @ id = 'dgn_content_de'] / div[2] / div[1] / div / table / tbody / tr / td / div / img")
->>>>>>> f11705b1a51c00f32cebc848dc6316266bd6317c
+        try:
+            prop = s.get('class')
+            if prop != None and prop[0] == "icon_pic_n":
+                print(s.get('href') + " : " + s.get_text())
+                url_info = s.get('href')
 
-                for img in imgs:
-                    imgurl = img.get_attribute("src")
-                    print(imgurl)
-<<<<<<< HEAD
+                if url_info[:4] != "http":
+                    url_info = "http://gall.dcinside.com" + url_info
 
-                    download_web_images(str(imgurl).encode())
-=======
+                print(url_info)
 
-                    download_web_images_noExtension(imgurl)
+                driver.get(url_info)
+                soup = BS(driver.page_source, 'html.parser')
 
->>>>>>> f11705b1a51c00f32cebc848dc6316266bd6317c
+                li = soup.find_all("li")
+                print(1)
+
+                for ele in li:
+                    print(2)
+                    val = ele.get('class')
+                    if val != None and val[0] == 'icon_pic':
+                        print(3)
+                        base_url = ele.a.get("href")
+                        rp_from = "http://image.dcinside.com/download.php"
+                        rp_to = "http://dcimg7.dcinside.com/viewimage.php"
+                        base_url = base_url.replace(rp_from, rp_to);
+                        print(base_url)
+                        f = open("./img/" + ele.a.contents[0], "wb");
+                        img_req = urllib.request.Request(base_url);
+                        f.write(urllib.request.urlopen(img_req).read())
+                        f.close()
+
+        except UnicodeEncodeError:
+            print("Error : " + str(idx))
+        finally:
+            idx += 1
+
+
+
+
+        # if ('href' in link.attrs and 'class' in link.attrs):
+        #     if(eq(str(link.attrs['class']),'[\'icon_pic_n\']')):
+        #         print(link.attrs['href'])
+        #
+        #         driver.find_element_by_xpath("// *[@href = \"" + link.attrs['href'] + "\"]").click()
+        #
+        #         time.sleep(1)
+        #
+        #         soup = BS(driver.page_source, 'html5lib')
+        #
+        #         imgs = soup.findAll("div", "app_editorno")
+        #         "// *[ @ id = 'dgn_content_de'] / div[2] / div[1] / div / table / tbody / tr / td / div[2] / img"
+        #
+        #         for img in imgs:
+        #
+        #             imgurl = img.get_attribute("app_editorno")
+        #             print(imgurl)
+
+
 
 
     driver.quit()
