1afbe73db42f100a61799e90a11a85daec98c24c
JiSoo <jijohn01@naver.com>
2017-09-01 02:03:58 +0900

    1.add url 2. make a list

--- Park/movie.py
+++ Park/movie.py
@@ -1,25 +1,46 @@
 from urllib.request import urlopen
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
-def getTitle(url):
-	try:
-		html = urlopen(url)
-	except HTTPError as e:
-		return None
-	
-	try:
-		bsObj = BeautifulSoup(html.read(), "html.parser")
-		#title = bsObj.findAll("dt").a
-		for title in bsObj.findAll("dt"):
-			#if 'title' in title.a:
-			print(title.a['title'])
-				
-	except AttributeError as e:
-		return None
-	return title
+import numpy as np
 
-title = getTitle("http://movie.naver.com/movie/preview/index.nhn")
-if title == None:
-	print("Title could not be found")
-else:
-	print("That is all")
+def main():
+    movies = getMovies("http://movie.naver.com/movie/preview/index.nhn")
+    print(np.array(scrapPreview(movies)))
+    '''
+    if title == None:
+        print("Title could not be found")
+    else:
+        print("That is all")
+    '''
+def getMovies(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    
+    try:
+        bsObj = BeautifulSoup(html.read(), "html.parser")
+        #for title in bsObj.findAll("dt"):
+        #	print(title.a['title'])
+        movies = bsObj.find('ul',attrs={'class':'prize_list'}).findAll('li')
+    except AttributeError as e:
+        return None
+    return movies
+    
+def scrapPreview(movies):
+    preview_list = []
+    naver_movie_url = "http://movie.naver.com"
+    labels = ['title','link']
+    preview_list.append(labels)
+    
+    for movie in movies:
+        preview_info = []
+        preview_info.append(movie.dl.dt.a['title'])
+        preview_info.append(naver_movie_url + movie.dl.dt.a['href'])
+       # print(movie.dl.dd.)
+        preview_list.append(preview_info)
+    
+    return preview_list
+    
+if __name__ == "__main__":
+    main()
