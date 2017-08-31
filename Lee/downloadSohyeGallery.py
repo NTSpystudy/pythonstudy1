01bc7d59116786ade8ffc36a568228c23e0b50c1
redtime2002@naver.com <redtime2002@naver.com>
2017-08-31 14:22:46 +0900

    ?

--- Lee/downloadSohyeGallery.py
+++ Lee/downloadSohyeGallery.py
@@ -1,6 +1,6 @@
 import random
 
-import urllib.request as url
+import urllib
 from bs4 import BeautifulSoup as BS
 
 from operator import eq
@@ -13,9 +13,30 @@ import time
 
 
 def download_web_images(url):
+    user_agent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
+
     name = random.randrange(1,1001)
     full_name = str(name) + ".jpg"
-    url.urlretrieve(url, "./img/" + full_name)
+
+    img_req = urllib.request.Request(url, data=None, headers={'User-Agent': user_agent});
+    urllib.request.urlretrieve(img_req, "./img/" + full_name)
+
+
+
+def download_web_images_no_extension(url):
+    user_agent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
+
+    name = random.randrange(1, 1001)
+    f = open("./img/" + str(name) + ".jpg", "wb");
+    img_req = urllib.request.Request(url, data=None, headers={'User-Agent': user_agent});
+
+    f.write(urllib.request.urlopen(img_req).read())
+    f.close()
+
+    #
+    # f = open( "./img/" + str(name) + '.jpg', 'wb')
+    # f.write(urllib.request.urlopen(url).read())
+    # f.close()
 
 
 if __name__ == "__main__":
@@ -32,14 +53,19 @@ if __name__ == "__main__":
         if ('href' in link.attrs and 'class' in link.attrs):
             if(eq(str(link.attrs['class']),'[\'icon_pic_n\']')):
                 print(link.attrs['href'])
-                driver.find_element_by_xpath("// *[@href = " + str(link.attrs['href']) + "]").click()
+                driver.find_element_by_xpath("// *[@href = \"" + str(link.attrs['href']) + "\"]").click()
 
                 time.sleep(1)
 
                 soup = BS(driver.page_source, 'html5lib')
-                src = driver.find_element_by_xpath("// *[ @ id = 'dgn_content_de''] / div[2] / div[1] / div / table / tbody / tr / td / div / img").get_attribute("src")
+                imgs = driver.find_elements_by_xpath(
+                    "// *[ @ id = 'dgn_content_de'] / div[2] / div[1] / div / table / tbody / tr / td / div / img")
 
-                print(src)
+                for img in imgs:
+                    imgurl = img.get_attribute("src")
+                    print(imgurl)
+
+                    download_web_images(str(imgurl).encode())
 
 
     driver.quit()
