086077ecb0fb35dab35a2d3e05f954305b7aaed7
JiSoo <jijohn01@naver.com>
2017-09-12 18:47:01 +0900

    start engel

---
+++ Park/engel.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def main():
+    account_book  = read_file("./account_book.csv")
+
+def read_file(file_name):
+    account_book = []
+    with open(file_name) as fp:
+        next(fp)
+        for line in fp:
+            
+    return 0;
+if __name__ == '__main__':
+    main()
