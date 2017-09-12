9c1c3941cd03ee25674eba9f5e6e1fce37d131c2
JiSoo <jijohn01@naver.com>
2017-09-12 19:05:03 +0900

    회사에서 한 것 까지 저장

--- Park/engel.py
+++ Park/engel.py
@@ -3,13 +3,17 @@ import numpy as np
 
 def main():
     account_book  = read_file("./account_book.csv")
+    print(account_book)
 
 def read_file(file_name):
     account_book = []
     with open(file_name) as fp:
         next(fp)
         for line in fp:
-            
-    return 0;
+            splitted = line.strip().split(',')
+            account_book.append(splitted)
+            print(splitted[0])
+
+    return np.array(account_book);
 if __name__ == '__main__':
     main()
