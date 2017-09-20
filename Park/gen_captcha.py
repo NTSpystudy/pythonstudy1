8ffd5d6a57212eabf5dc707436c79d92729a91d7
JiSoo <jijohn01@naver.com>
2017-09-20 17:37:50 +0900

    gen_captcha 를 개선(색상과 문자를 랜덤하게 생성, 파일이름과 캡차 정답 일치, 파일 저장 위치 변경)

--- Park/gen_captcha.py
+++ Park/gen_captcha.py
@@ -9,23 +9,20 @@ from tempfile import NamedTemporaryFile
 
 PWD = os.getcwd()
 SEP = os.path.sep
-FONT_FILE = SEP.join([PWD, "veramono.ttf"])
+FONT_FILE = SEP.join([PWD, "VeraMono.ttf"])
 FONT_SIZE = 35
-POOL = 'acbdefghijklmnopqrstuvwxyz1234567890'
-
+POOL = "1234567890ABCDEFGHIJKLNMOPQRSTUVWXYZ"
 
 LENGTH = 6
 WIDTH = 135
 HEIGHT = 50
 
-R = 00
-G = 39
-B = 50
+R = random.randint(0,256)
+G = random.randint(0,256)
+B = random.randint(0,256)
 
 def generate_captcha(PATH=PWD):
-    """Author:
-        haandol <ldg55d@gmail.com>
-
+    """
     Parameters:
         PATH – path to where captha image will be stored
 
@@ -36,16 +33,24 @@ def generate_captcha(PATH=PWD):
         if you want to encrypting captcha library,
         you should find other library like python-recaptcha or something.
     """
-    with NamedTemporaryFile(suffix='.jpg', dir=PATH, delete=False) as fp:
+    """Modified by JiSoo
+        1. Characters and Colors are generated randomly
+        2. image file name and value are equal
+        3. Captcha file dir is changed(captcha_image)"""
+    with NamedTemporaryFile(suffix='.jpg', dir=PATH,delete=False) as fp:
         im = Image.new("RGB", (WIDTH, HEIGHT), (R, G, B))
         draw = ImageDraw.Draw(im)
         font = ImageFont.truetype(FONT_FILE, FONT_SIZE, encoding='unic')
 
         value = "".join(random.sample(POOL, LENGTH))
-        #value = 'kimyongyeon'
-        draw.text((5, 1), value, font=font)
+        #value = "354DD5"
+        draw.text((5, 1), value, font=font,fill=(256-R,256-G,256-B))
 
-        im.save(fp.name)
+        #im.save(fp.name)
+        im.save("./captcha_image/"+value+".jpg")
+        im.show()
+        fp.close()
+        os.remove(fp.name)#임시파일 생성된 것을 삭제해줌
         return im, value
 
 if __name__ == '__main__':
