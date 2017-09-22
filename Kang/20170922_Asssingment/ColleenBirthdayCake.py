352c672c0c4e040b89eb65d52fb1829386efa451
minkyuinkorea <31121180+minkyuinkorea@users.noreply.github.com>
2017-09-22 12:37:06 +0900

    과제ㅎㅎ
    과제ㅎㅎ

---
+++ Kang/20170922_Asssingment/ColleenBirthdayCake.py
@@ -0,0 +1,11 @@
+def birthdayCake(heights):
+    max_ = max(heights)
+    count = 0
+    for height in heights:
+        if height == max_:
+            count +=1
+    print("Colleen blowed %d candles"%count)
+num = int(input("Number of Candle : ").strip())
+heights = list(map(int, input("Enter each candle's height : ").strip().split(' ')))
+
+result = birthdayCake(heights)
